c692283100f7bc4d6678b5d0ceddca71da82c813
GilLópezVíctor <vgil25@ilb.cat>
2026-03-25 12:27:33 +0100

    Add <tittle> & <h1>

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>0376-RA5PR1-GilLopezVictor</title>
 </head>
 <body>
+    <h1> TABLA DE MULTIPLICAR </h1>
     
 </body>
 </html>
