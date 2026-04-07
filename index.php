db8f4e7e85d304c1c399afd85f4f795e010cab9a
GilLópezVíctor <vgil25@ilb.cat>
2026-04-07 09:58:39 +0200

    Feat formulario GET para introducir el número dinámicamente

--- index.php
+++ index.php
@@ -1,71 +1,67 @@
-<?php
-$numero = 7;
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>0376-RA5PR1-GilLopezVictor</title>
     <style>
-    
-    h1{
-    	text-align: center;
+        h1 {
+            text-align: center;
         }
-        
-    table { 
-    	border-collapse: collapse;
-        margin: auto;
-    	 
-    	}
-        
+        table { 
+            border-collapse: collapse;
+            margin: auto;
+        }
         td { 
             border: 1px solid black;
             padding: 15px; 
         }
-        
         th {
             border: 1px solid black;
             padding: 15px;
             background-color: lightblue;
-        	}
-            
+        }
         tr.par { 
             background-color: lightgray; 
-        	}
-            
+        }
         tr.impar { 
             background-color: white; 
         }
-        
     </style>
 </head>
 <body>
 
-    <h1> TABLA DE MULTIPLICAR DEL <?php echo $numero; ?></h1>
+<form method="get" action="index.php">
+    <label>Introduce un número:</label>
+    <input type="number" name="numero" min="1" max="12">
+    <button type="submit">Generar tabla</button>
+</form>
 
-    <?php
-	echo "<table>";
-	echo "<tr><th>Operacion</th><th>Resultado</th></tr>";
+<?php
+if (isset($_GET['numero'])) {
 
-	for ($i = 1; $i <= 10; $i++) {
-    
-    	if ($i % 2 == 0) {
+    $numero = (int)$_GET['numero'];
+
+    echo "<h1>TABLA DE MULTIPLICAR DEL $numero</h1>";
+    echo "<table>";
+    echo "<tr><th>Operacion</th><th>Resultado</th></tr>";
+
+    for ($i = 1; $i <= 10; $i++) {
+
+        if ($i % 2 == 0) {
             $clase = "par";
         } else {
             $clase = "impar";
         }
-        
-        $resultado = $numero * $i;
-        
-    	echo "<tr class= '$clase'>";
-  	  	echo "<td>$numero x $i</td>";
-    	echo "<td>$resultado</td>";
-    	echo "</tr>";
-	}
 
-	echo "</table>";
+        $resultado = $numero * $i;
+
+        echo "<tr class='$clase'>";
+        echo "<td>$numero x $i</td>";
+        echo "<td>$resultado</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+}
 ?>
 
 </body>
